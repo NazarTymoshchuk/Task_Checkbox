257ede79abc47f4f3a7a1852313673f7b7000713
Nazar <Timo_qm01@student.itstep.org>
2023-05-05 15:01:49 +0300

    Add enum and method for print

--- Task_Checkbox/Program.cs
+++ Task_Checkbox/Program.cs
@@ -1,4 +1,11 @@
-﻿class Checkbox
+﻿enum ICONS
+{
+	CHECKED = (char)251,
+	UNCHECKED = (char)233,
+	INDETERMINATE = (char)254
+}
+
+class Checkbox
 {
     public bool? IsChecked { get; set; }
 	public bool IsEnable { get; set; }
@@ -23,13 +30,23 @@
 		if(IsEnable)
 		{
 			if(IsIndeterminate)
-			{
 				IsChecked = null;
-				return;
-			}
+			else
+				IsChecked = isChecked;
 
-			IsChecked = isChecked;
+			Print(isChecked);
 		}
 	}
 
+	private void Print(bool? isChecked) 
+	{
+		if(isChecked == true)
+			Console.WriteLine(ICONS.CHECKED + " : " + Title);
+
+        if (isChecked == false)
+            Console.WriteLine(ICONS.UNCHECKED + " : " + Title);
+
+        if (isChecked == null)
+            Console.WriteLine(ICONS.INDETERMINATE + " : " + Title);
+    }
 }

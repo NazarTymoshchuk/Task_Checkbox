7eed047a1a4aa618e97d82b10263d16c1744a966
Nazar <Timo_qm01@student.itstep.org>
2023-05-05 14:50:19 +0300

    Add properties

--- Task_Checkbox/Program.cs
+++ Task_Checkbox/Program.cs
@@ -1,9 +1,22 @@
 ﻿class Checkbox
 {
     public bool IsChecked { get; set; }
+	public bool IsEnable { get; set; }
+	public bool IsIndeterminate { get; set; }
+	public string Title { get; set; }
 
 	public Checkbox()
 	{
 		IsChecked = false;
+		IsEnable = true;
+		IsIndeterminate = false;
+		Title = string.Empty;
 	}
+
+	public Checkbox(string title) : this()
+	{
+		Title = title;
+	}
+
+
 }

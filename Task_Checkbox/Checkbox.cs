db05102bca513ba10f60da175ecab2d2e20d8d6a
Nazar <Timo_qm01@student.itstep.org>
2023-05-05 21:29:00 +0300

    Move checkbox class to a separate file

---
+++ Task_Checkbox/Checkbox.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+
+class Checkbox : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+	public bool IsEnable { get; set; }
+	public bool? IsChecked { get; private set; }
+	public bool CanBeIndeterminate { get; set; }
+	public string Title { get; set; }
+
+	public Checkbox()
+	{
+		IsChecked = false;
+		IsEnable = true;
+		CanBeIndeterminate = false;
+		Title = string.Empty;
+	}
+
+	public Checkbox(string title) : this()
+	{
+		Title = title;
+	}
+
+    public void Click()
+	{
+		if (IsEnable)
+		{
+			if (IsChecked == true)
+				IsChecked = false;
+
+			else if (IsChecked == false && CanBeIndeterminate)
+				IsChecked = null;
+
+			else if (IsChecked == false)
+				IsChecked = true;
+		}
+	}
+
+	public char Icon
+	{
+		get
+		{
+			if (IsChecked == true)
+				return (char)ICONS.CHECKED;
+			else if (IsChecked == false)
+                return (char)ICONS.UNCHECKED;
+			else 
+				return (char)ICONS.INDETERMINATE;
+        }
+	}
+	public void Print() => Console.WriteLine(Icon + " : " + Title);
+
+    public override string ToString() => Icon + " : " + Title;
+
+    public static implicit operator Checkbox(bool value) => new Checkbox() { IsChecked = value };
+
+	public static bool operator true(Checkbox checkbox) => checkbox.IsChecked == true ? true : false;
+
+    public static bool operator false(Checkbox checkbox) => checkbox.IsChecked == false ? true : false;
+}
--- Task_Checkbox/Program.cs
+++ Task_Checkbox/Program.cs
@@ -1,67 +1,14 @@
-﻿using System.ComponentModel;
-
-enum ICONS
+﻿enum ICONS
 {
 	CHECKED = (char)84,
 	UNCHECKED = (char)70,
 	INDETERMINATE = (char)73
 }
 
-class Checkbox : INotifyPropertyChanged
+public class Program
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-	public bool IsEnable { get; set; }
-	public bool? IsChecked { get; private set; }
-
-	public bool CanBeIndeterminate { get; private set; }
-	public string Title { get; set; }
-
-	public Checkbox()
-	{
-		IsChecked = false;
-		IsEnable = true;
-		CanBeIndeterminate = false;
-		Title = string.Empty;
-	}
-
-	public Checkbox(string title) : this()
-	{
-		Title = title;
-	}
-
-
-    public void Checked(bool? isChecked)
-	{
-		if (IsEnable)
-		{
-			if (CanBeIndeterminate)
-				IsChecked = null;
-			else
-				IsChecked = isChecked;
-
-			Console.WriteLine(Print());
-		}
-	}
-
-	public string Print()
-	{
-		if (IsChecked == true)
-			return ((char)ICONS.CHECKED) + " : " + Title;
-
-		if (IsChecked == false)
-            return ((char)ICONS.UNCHECKED) + " : " + Title;
-
-		if (IsChecked == null)
-            return ((char)ICONS.INDETERMINATE) + " : " + Title;
-
-		return string.Empty;
-	}
-
-    public override string ToString()
+    public static void Main()
     {
-		return Print();
+		
     }
-
-    public static implicit operator Checkbox(bool value) => new Checkbox() { IsChecked = value };
 }
